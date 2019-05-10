e0b808d5a4f08a0a37d76c9c533336dce0cc1a5f
Paul Martin <communty.hivemind@gmail.com>
2019-05-10 11:27:35 +0100

    Refactor three methods into 1

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,171 +1,173 @@
-var {Shop, Item} = require('../src/gilded_rose.js');
-describe("Gilded Rose", function() {
-  var gildedRose, items;
+var { Shop, Item } = require('../src/gilded_rose.js')
+describe('Gilded Rose', function () {
+  var gildedRose, items
 
-  function c (thing) {
-    console.log(thing);
-  };
-
-  describe("updating a normal object", function () {
+  describe('updating a normal object', function () {
     beforeEach(function () {
-      gildedRose = new Shop([ new Item("foo", 10, 10) ]);
-      items = gildedRose.updateQuality();
+      gildedRose = new Shop([ new Item('foo', 10, 10) ])
+      items = gildedRose.updateQuality()
     })
 
-    it("has a name", function() {
-      expect(items[0].name).toEqual("foo");
-    });
+    it('has a name', function () {
+      expect(items[0].name).toEqual('foo')
+    })
 
-    it("the quality decreases by 1", function() {
-      expect(items[0].quality).toEqual(9);
-    });
+    it('the quality decreases by 1', function () {
+      expect(items[0].quality).toEqual(9)
+    })
 
-    it("the sell by date decreases by 1", function() {
-      expect(items[0].sellIn).toEqual(9);
-    });
+    it('the sell by date decreases by 1', function () {
+      expect(items[0].sellIn).toEqual(9)
+    })
 
-    it("once the sell by date is reached it degrades twice as fast", function () {
-      gildedRose = new Shop([ new Item("foo", 0, 10) ]);
-      items = gildedRose.updateQuality();
-      expect(items[0].quality).toEqual(8);
-    });
+    it('once the sell by date is reached it degrades twice as fast', function () {
+      gildedRose = new Shop([ new Item('foo', 0, 10) ])
+      items = gildedRose.updateQuality()
+      expect(items[0].quality).toEqual(8)
+    })
 
     it("the quality can't decrease below zero", function () {
-      gildedRose = new Shop([ new Item("foo", 10, 0) ]);
-      items = gildedRose.updateQuality();
-      expect(items[0].quality).toEqual(0);
+      gildedRose = new Shop([ new Item('foo', 10, 0) ])
+      items = gildedRose.updateQuality()
+      expect(items[0].quality).toEqual(0)
     })
-  });
+  })
 
-  describe("updating a conjured object", function () {
+  describe('updating a conjured object', function () {
     beforeEach(function () {
-      gildedRose = new Shop([ new Item("Conjured item", 10, 10) ]);
-      items = gildedRose.updateQuality();
+      gildedRose = new Shop([ new Item('Conjured item', 10, 10) ])
+      items = gildedRose.updateQuality()
     })
 
-    it("has a name", function() {
-      expect(items[0].name).toEqual("Conjured item");
-    });
+    it('has a name', function () {
+      expect(items[0].name).toEqual('Conjured item')
+    })
 
-    it("the quality decreases by 2", function() {
-      expect(items[0].quality).toEqual(8);
-    });
+    it('the quality decreases by 2', function () {
+      expect(items[0].quality).toEqual(8)
+    })
 
-    it("the sell by date decreases by 1", function() {
-      expect(items[0].sellIn).toEqual(9);
-    });
+    it('the sell by date decreases by 1', function () {
+      expect(items[0].sellIn).toEqual(9)
+    })
 
-    it("once the sell by date is reached it degrades twice as fast", function () {
-      gildedRose = new Shop([ new Item("Conjured item", 0, 10) ]);
-      items = gildedRose.updateQuality();
-      expect(items[0].quality).toEqual(6);
-    });
+    it('once the sell by date is reached it degrades twice as fast', function () {
+      gildedRose = new Shop([ new Item('Conjured item', 0, 10) ])
+      items = gildedRose.updateQuality()
+      expect(items[0].quality).toEqual(6)
+    })
 
     it("the quality can't decrease below zero", function () {
-      gildedRose = new Shop([ new Item("Conjured item", 10, 0) ]);
-      items = gildedRose.updateQuality();
-      expect(items[0].quality).toEqual(0);
+      gildedRose = new Shop([ new Item('Conjured item', 10, 0) ])
+      items = gildedRose.updateQuality()
+      expect(items[0].quality).toEqual(0)
     })
-  });
-  describe("updating Aged Brie", function () {
+  })
+
+  describe('updating Aged Brie', function () {
     beforeEach(function () {
-      gildedRose = new Shop([ new Item("Aged Brie", 10, 10) ]);
-      items = gildedRose.updateQuality();
+      gildedRose = new Shop([ new Item('Aged Brie', 10, 10) ])
+      items = gildedRose.updateQuality()
     })
 
-    it("has a name", function() {
-      expect(items[0].name).toEqual("Aged Brie");
-    });
+    it('has a name', function () {
+      expect(items[0].name).toEqual('Aged Brie')
+    })
 
-    it("the quality increases by 1", function() {
-      expect(items[0].quality).toEqual(11);
-    });
+    it('the quality increases by 1', function () {
+      expect(items[0].quality).toEqual(11)
+    })
 
-    it("the sell by date decreases by 1", function() {
-      expect(items[0].sellIn).toEqual(9);
-    });
+    it('the quality increases by 2 after the sell by date', function () {
+      gildedRose = new Shop([ new Item('Aged Brie', 0, 10) ])
+      items = gildedRose.updateQuality()
+      expect(items[0].quality).toEqual(12)
+    })
 
-    it("the quality can't increase beyond 50", function() {
-      gildedRose = new Shop([ new Item("Aged Brie", 10, 50) ]);
-      items = gildedRose.updateQuality();
-      expect(items[0].quality).toEqual(50);
-    });
-  });
+    it('the sell by date decreases by 1', function () {
+      expect(items[0].sellIn).toEqual(9)
+    })
 
-  describe("updating Sulfuras, Hand of Ragnaros", function () {
+    it("the quality can't increase beyond 50", function () {
+      gildedRose = new Shop([ new Item('Aged Brie', 10, 50) ])
+      items = gildedRose.updateQuality()
+      expect(items[0].quality).toEqual(50)
+    })
+  })
+
+  describe('updating Sulfuras, Hand of Ragnaros', function () {
     beforeEach(function () {
-      gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 10, 10) ]);
-      items = gildedRose.updateQuality();
-    });
+      gildedRose = new Shop([ new Item('Sulfuras, Hand of Ragnaros', 10, 10) ])
+      items = gildedRose.updateQuality()
+    })
 
-    it("has a name", function() {
-      expect(items[0].name).toEqual("Sulfuras, Hand of Ragnaros");
-    });
+    it('has a name', function () {
+      expect(items[0].name).toEqual('Sulfuras, Hand of Ragnaros')
+    })
 
-    it("the quality does not change", function() {
-      expect(items[0].quality).toEqual(10);
-    });
+    it('the quality does not change', function () {
+      expect(items[0].quality).toEqual(10)
+    })
 
-    it("the sell by date does not change", function() {
-      expect(items[0].sellIn).toEqual(10);
-    });
-  });
+    it('the sell by date does not change', function () {
+      expect(items[0].sellIn).toEqual(10)
+    })
+  })
 
-  describe("updating Backstage passes...", function () {
-    it("has a name", function() {
-      gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10) ]);
-      items = gildedRose.updateQuality();
-      expect(items[0].name).toEqual("Backstage passes to a TAFKAL80ETC concert");
-    });
+  describe('updating Backstage passes...', function () {
+    it('has a name', function () {
+      gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 10, 10) ])
+      items = gildedRose.updateQuality()
+      expect(items[0].name).toEqual('Backstage passes to a TAFKAL80ETC concert')
+    })
 
-    it("the sell by date decreases by 1", function() {
-      gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10) ]);
-      items = gildedRose.updateQuality();
-      expect(items[0].sellIn).toEqual(9);
-    });
+    it('the sell by date decreases by 1', function () {
+      gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 10, 10) ])
+      items = gildedRose.updateQuality()
+      expect(items[0].sellIn).toEqual(9)
+    })
 
-    it("the quality increases by 1 if sellIn > 10", function() {
-      gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 11, 10) ]);
-      items = gildedRose.updateQuality();
-      expect(items[0].quality).toEqual(11);
-    });
+    it('the quality increases by 1 if sellIn > 10', function () {
+      gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 11, 10) ])
+      items = gildedRose.updateQuality()
+      expect(items[0].quality).toEqual(11)
+    })
 
-    it("the quality increases by 2 if sellIn = 10", function() {
-      gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10) ]);
-      items = gildedRose.updateQuality();
-      expect(items[0].quality).toEqual(12);
-    });
+    it('the quality increases by 2 if sellIn = 10', function () {
+      gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 10, 10) ])
+      items = gildedRose.updateQuality()
+      expect(items[0].quality).toEqual(12)
+    })
 
-    it("the quality increases by 2 if sellIn = 6", function() {
-      gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 6, 10) ]);
-      items = gildedRose.updateQuality();
-      expect(items[0].quality).toEqual(12);
-    });
+    it('the quality increases by 2 if sellIn = 6', function () {
+      gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 6, 10) ])
+      items = gildedRose.updateQuality()
+      expect(items[0].quality).toEqual(12)
+    })
 
-    it("the quality increases by 3 if sellIn = 5", function() {
-      gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10) ]);
-      items = gildedRose.updateQuality();
-      expect(items[0].quality).toEqual(13);
-    });
+    it('the quality increases by 3 if sellIn = 5', function () {
+      gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 5, 10) ])
+      items = gildedRose.updateQuality()
+      expect(items[0].quality).toEqual(13)
+    })
 
-    it("the quality increases by 3 if sellIn = 1", function() {
-      gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 1, 10) ]);
-      items = gildedRose.updateQuality();
-      expect(items[0].quality).toEqual(13);
-    });
-    
-    it("the quality drops to 0 if sellIn = 0", function() {
-      gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10) ]);
-      items = gildedRose.updateQuality();
-      expect(items[0].quality).toEqual(0);
-    });
+    it('the quality increases by 3 if sellIn = 1', function () {
+      gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 1, 10) ])
+      items = gildedRose.updateQuality()
+      expect(items[0].quality).toEqual(13)
+    })
 
-    it("the quality doesn't drop below 0", function() {
-      gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10) ]);
-      items = gildedRose.updateQuality();
-      items = gildedRose.updateQuality();
-      expect(items[0].quality).toEqual(0);
-    });
-  });
+    it('the quality drops to 0 if sellIn = 0', function () {
+      gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10) ])
+      items = gildedRose.updateQuality()
+      expect(items[0].quality).toEqual(0)
+    })
 
-});
+    it("the quality doesn't drop below 0", function () {
+      gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10) ])
+      items = gildedRose.updateQuality()
+      items = gildedRose.updateQuality()
+      expect(items[0].quality).toEqual(0)
+    })
+  })
+})
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,60 +1,58 @@
 class Item {
-  constructor(name, sellIn, quality){
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
+  constructor (name, sellIn, quality) {
+    this.name = name
+    this.sellIn = sellIn
+    this.quality = quality
   }
 }
 
 class Shop {
-  constructor(items=[]){
-    this.items = items;
+  constructor (items = []) {
+    this.items = items
   }
 
-  updateQuality() {
-    var that = this;
-    this.items.forEach(function(item) {
-
+  updateQuality () {
+    var that = this
+    this.items.forEach(function (item) {
       switch (item.name) {
-        case "Sulfuras, Hand of Ragnaros":
-          break;
-        case "Aged Brie":
-          that._updateQualityAgedBrie(item);
-          break;
-        case "Backstage passes to a TAFKAL80ETC concert":
+        case 'Sulfuras, Hand of Ragnaros':
+          break
+        case 'Aged Brie':
+          that._updateQualitySimpleItem(item, 1)
+          break
+        case 'Backstage passes to a TAFKAL80ETC concert':
           that._updateQualityBackstagePass(item)
-          break;
-        case "Conjured item":
-          that._updateQualityConjuredItem(item);
-          break;
+          break
+        case 'Conjured item':
+          that._updateQualitySimpleItem(item, -2)
+          break
         default:
-          that._updateQualityNormalItem(item)
+          that._updateQualitySimpleItem(item, -1)
       }
 
       that._updateSellIn(item)
+    })
 
-    });
-
-    return this.items;
+    return this.items
   }
 
-  _updateSellIn(item) {
-    if (item.name != 'Sulfuras, Hand of Ragnaros') {
-      item.sellIn -= 1;
+  _updateSellIn (item) {
+    if (item.name !== 'Sulfuras, Hand of Ragnaros') {
+      item.sellIn -= 1
     }
   }
 
-  _updateQualityAgedBrie(item) {
-    if (item.sellIn <= 0) {
-      this._incrementQuality(item, 2)
+  _updateQualitySimpleItem (item, amount) {
+    if (item.sellIn < 1) {
+      this._incrementQuality(item, 2 * amount)
     } else {
-      this._incrementQuality(item, 1)
+      this._incrementQuality(item, amount)
     }
   }
 
-  _updateQualityBackstagePass(item) {
+  _updateQualityBackstagePass (item) {
     if (item.sellIn <= 0) {
-      item.quality = 0;
+      item.quality = 0
     } else if (item.sellIn > 0 && item.sellIn <= 5) {
       this._incrementQuality(item, 3)
     } else if (item.sellIn > 5 && item.sellIn <= 10) {
@@ -64,23 +62,7 @@ class Shop {
     }
   }
 
-  _updateQualityConjuredItem(item) {
-    if (item.sellIn < 1) {
-      this._incrementQuality(item, -4)
-    } else {
-      this._incrementQuality(item, -2)
-    }
-  }
-
-  _updateQualityNormalItem(item) {
-    if (item.sellIn < 1) {
-      this._incrementQuality(item, -2)
-    } else {
-      this._incrementQuality(item, -1)
-    }
-  }
-
-  _incrementQuality(item, amount) {
+  _incrementQuality (item, amount) {
     item.quality += amount
     item.quality = Math.min(item.quality, 50)
     item.quality = Math.max(item.quality, 0)
